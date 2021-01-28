2d18850145d5b03382613bb938dc9bc2cda88d6a
ravidugm <ravindumiyuranga@gmail.com>
2021-01-28 18:17:41 +0530

    OOP Practices

--- src/lectures/part02oop/OOBasics.scala
+++ src/lectures/part02oop/OOBasics.scala
@@ -7,6 +7,26 @@ object OOBasics extends App {
 
   person.greeting("Miyu")
   person.greeting()
+
+  val writer = new Writer("John", "Doe", 2000)
+  writer.fullName
+
+  val author = new Writer("Kumarathunga", "Munidasa", 1941)
+  val imposter = new Writer("Kumarathunga", "Munidasa", 1941)
+  val novel = new Novel("Kaliyuga", 1955, author)
+
+  println(novel.authorAge)
+  println(novel.isWrittenBy(author))
+  println(novel.isWrittenBy(imposter))
+
+  val counter = new Counter
+  counter.inc.print
+  counter.inc.inc.inc.print
+  counter.inc(10).print
+
+  counter.dec.print
+  counter.dec.dec.dec.print
+  counter.dec(10).print
 }
 
 // Constructor
@@ -26,6 +46,62 @@ class Person(name: String, val age: Int = 0) {
   def this(name: String) = this(name, 0)
 
   def this() = this("John Doe")
+}
+
+/*
+  Novel and a Writer
+
+  Writer: first name, surname, year
+    -method fullname
+
+  Novel: name, year of release, author
+    -authorAge
+    -isWrittenBy (author)
+    -copy (new year of release) = new instance of Novel
+ */
+
+class Writer(firstName: String, surName: String, val birthYear: Int) {
+  def fullName: String = firstName + " " + surName
+}
+
+class Novel(novelName: String, releaseYear: Int, author: Writer) {
+  def authorAge: Int = releaseYear - author.birthYear
+
+  def isWrittenBy(author: Writer) = author == this.author
+
+  def releaseCopy(newYear: Int): Novel = new Novel(novelName, newYear, author)
+}
+
+/*
+  Counter class
+    -receives an int value
+    -method current count
+    -method to increment/decrement => new Counter
+    -overload increment/decrement to receive an amount
+ */
+
+class Counter(val count: Int = 0) { // If parameter is return that Field we use val
+  def inc = {
+    println("Incrementing")
+    new Counter(count + 1)
+  } // Immutability
+
+  def dec = {
+    println("Decrementing")
+    new Counter(count - 1)
+  }
+
+  def inc(n: Int): Counter = {
+    if (n <= 0) this
+    else inc.inc(n - 1)
+  }
+
+  def dec(n: Int): Counter = {
+    if (n <= 0) this
+    else dec.dec(n - 1)
+  }
+
+  def print = println(count)
 }
 
 // Class parameters are NOT FIELDS
